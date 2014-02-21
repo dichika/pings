5057f693a909f12d6c0f9f39e7d30d9262a557fa
dichika <ichikawa@hcc-jp.com>
2014-02-21 22:46:49 +0900

    change calculation of time

--- R/pings.R
+++ R/pings.R
@@ -1,10 +1,10 @@
 pings <- function(x, interval=0.25, countp=2, endp=8){
   require(dplyr)
   require(pingr)
-  time <- strsplit(as.character(substitute(x)), split="%.%", fixed=TRUE) %.% unlist() %.% length()
+  time <- substitute(x) %.% paste(collapse="") %.% gregexpr(pattern="%.%") %.% unlist() %.% length()
   i <- 0
   repeat {
-    if (i <= time){
+    if (i < time){
       i <- i+1
       ping(countp)
       Sys.sleep(interval)
