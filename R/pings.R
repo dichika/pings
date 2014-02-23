63f7eacbf00b824b8a0cbdec0327acdd8c20dcc2
dichika <ichikawa@hcc-jp.com>
2014-02-23 17:43:37 +0900

    add error handlings & change messages

--- R/pings.R
+++ R/pings.R
@@ -1,20 +1,35 @@
 pings <- function(x, interval=0.25, countp=2, endp=8){
   require(dplyr)
   require(pingr)
-  time <- substitute(x) %.% paste(collapse="") %.% gregexpr(pattern="%.%") %.% unlist() %.% length()
-  i <- 0
-  repeat {
-    if (i < time){
-      i <- i+1
-      ping(countp)
-      Sys.sleep(interval)
-    }else{
-      Sys.sleep(interval)
-      ping(endp)
-      Sys.sleep(interval)
-      break
+  res <- substitute(x) %.% paste(collapse="") %.% gregexpr(pattern="%.%") %.% unlist()
+  time <- res %.% length()
+  if(res[1] != -1){
+    tryCatch({
+      eval(x)
+    },
+    error = function(e){
+      message(e)
+      stop(call.=FALSE, domain=NA)
+    })
+    i <- 0
+    repeat {
+      if (i < time){
+        i <- i+1
+        ping(countp)
+        Sys.sleep(interval)
+      }else{
+        Sys.sleep(interval)
+        ping(endp)
+        Sys.sleep(interval)
+        break
+      }
     }
-  }
-  cat(sprintf("\n\n!!!!! %s Hit Combo !!!!!\n\n", time))
-  return(eval(x))
-}
+    if(time>=5){
+           cat(sprintf("\n\n!!!!! %s Hits Combo !!!!!\n!!!!! Excellent !!!!!\n", time))
+    } else if(time>=2){
+      cat(sprintf("\n\n!!!!! %s Hits Combo !!!!!\n!!!!! Good !!!!!\n", time))      
+    } else 
+      cat(sprintf("\n\n      Only %s Hit :( \n\n", time))
+} else {
+    return(eval(x))
+}}
