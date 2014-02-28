5df3e902f148e8373abc25888458c19288f6c9c8
dichika <ichikawa@hcc-jp.com>
2014-02-28 21:32:02 +0900

    add some

--- R/pings.R
+++ R/pings.R
@@ -33,3 +33,39 @@ pings <- function(x, interval=0.25, countp=2, endp=8){
 } else {
     return(eval(x))
 }}
+
+pings2 <- function(x, interval=0.25){
+  require(dplyr)
+  require(pingr)
+  res <- substitute(x) %.% paste(collapse="") %.% gregexpr(pattern="%.%") %.% unlist()
+  time <- res %.% length()
+  if(res[1] != -1){
+    tryCatch({
+      eval(x)
+    },
+    error = function(e){
+      message(e)
+      stop(call.=FALSE, domain=NA)
+    })
+    i <- 0
+    repeat {
+      if (i < time){
+        i <- i+1
+        ping2(path=system.file(paste("sounds/", "a_02.wav", sep = ""), package = "pingr"))
+        Sys.sleep(interval)
+      }else{
+        Sys.sleep(interval)
+        ping2(path=system.file(paste("sounds/", "tsuzumi.wav", sep = ""), package = "pingr"))
+        Sys.sleep(interval)
+        break
+      }
+    }
+    if(time>=5){
+      cat(sprintf("\n\n!!!!! %s Hits Combo !!!!!\n!!!!! Excellent !!!!!\n", time))
+    } else if(time>=2){
+      cat(sprintf("\n\n!!!!! %s Hits Combo !!!!!\n!!!!! Good !!!!!\n", time))      
+    } else 
+      cat(sprintf("\n\n      Only %s Hit :( \n\n", time))
+  } else {
+    return(eval(x))
+  }}
